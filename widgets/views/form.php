5ec5afe9fe175d474aa3e86a33bdb452ace13e16
Jean CARALY <info@technical-blue.com>
2020-04-07 12:47:44 +0200

    Commente l'astuce en dessous en dessous du champs d'envoi à propos du markdown

--- widgets/views/form.php
+++ widgets/views/form.php
@@ -18,6 +18,8 @@ AnnouncementsAsset::register($this);
 ?>
 
 <!-- Show hints -->
+<!-- 
 <div class="contentForm_options" data-content-component="announcements.Announcement">
     <p class="help-block"><?= Yii::t('AnnouncementsModule.widgets', 'Note: You can use markdown syntax. (For more information visit <a target="_blank" href="https://github.com/adam-p/markdown-here/wiki/Markdown-Cheatsheet">this link</a>)'); ?></p>
 </div>
+-->
