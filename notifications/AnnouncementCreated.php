c946cb2c0691e726e9023394ab8f1de958a3c656
Jean CARALY <info@technical-blue.com>
2020-04-10 10:56:53 +0200

    changement textuel sur les envois de mails

--- notifications/AnnouncementCreated.php
+++ notifications/AnnouncementCreated.php
@@ -44,10 +44,10 @@ class AnnouncementCreated extends BaseNotification
      */
     public function html()
     {
-        return Yii::t('AnnouncementsModule.notifications', '{displayName} : "{subject}..." espace {spaceName}.', [
+        return Yii::t('AnnouncementsModule.notifications', '{displayName} : "{subject}..." dans l\'espace {spaceName}.', [
             'displayName' => Html::tag('strong', Html::encode($this->originator->displayName)),
             'spaceName' =>  Html::tag('strong',Html::encode($this->source->content->container->displayName)),
-            'subject' =>  substr(Html::encode($this->source->content),0,35)
+            'subject' =>  substr($this->source->content,0,35)
         ]);
     }
 
@@ -56,10 +56,10 @@ class AnnouncementCreated extends BaseNotification
      */
     public function getMailSubject()
     {
-        return Yii::t('AnnouncementsModule.notifications', '{displayName} : "{subject}..." espace {spaceName}.', [
+        return Yii::t('AnnouncementsModule.notifications', '{displayName} : "{subject}..." dans l\'espace {spaceName}.', [
             'displayName' => Html::tag('strong', Html::encode($this->originator->displayName)),
             'spaceName' =>  Html::tag('strong',Html::encode($this->source->content->container->displayName)),
-            'subject' =>  substr(Html::encode($this->source->content),0,35)
+            'subject' =>  substr($this->source->content,0,35)
         ]);
     }
 }
