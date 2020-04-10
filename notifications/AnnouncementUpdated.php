516670307e50f0dd1f2c63110770cc8fde6b07ba
Jean CARALY <info@technical-blue.com>
2020-04-10 10:52:26 +0200

    changement textuel sur les envois de mails

--- notifications/AnnouncementUpdated.php
+++ notifications/AnnouncementUpdated.php
@@ -44,7 +44,7 @@ class AnnouncementUpdated extends BaseNotification
      */
     public function html()
     {
-        return Yii::t('AnnouncementsModule.notifications', '{displayName} : "{subject}..." dans l\'espace {spaceName}.', [
+        return Yii::t('AnnouncementsModule.notifications', '{displayName} : "{subject}..." espace {spaceName}.', [
             'displayName' => Html::tag('strong', Html::encode($this->originator->displayName)),
             'spaceName' =>  Html::tag('strong',Html::encode($this->source->content->container->displayName)),
             'subject' =>  substr(Html::encode($this->source->content),0,35)
@@ -56,7 +56,7 @@ class AnnouncementUpdated extends BaseNotification
      */
     public function getMailSubject()
     {
-        return Yii::t('AnnouncementsModule.notifications', '{displayName} : "{subject}..." dans l\'espace {spaceName}.', [
+        return Yii::t('AnnouncementsModule.notifications', '{displayName} : "{subject}..." espace {spaceName}.', [
             'displayName' => Html::tag('strong', Html::encode($this->originator->displayName)),
             'spaceName' =>  Html::tag('strong',Html::encode($this->source->content->container->displayName)),
             'subject' =>  substr(Html::encode($this->source->content),0,35)
